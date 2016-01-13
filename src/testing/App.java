14ec71ea9f67cecf56902157a0887df4c4fa0ed2
Reuben Tshehla <tshegofatso100@gmail.com>
2016-01-13 13:21:43 +0200

    added a new method

--- src/testing/App.java
+++ src/testing/App.java
@@ -5,6 +5,12 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub 
 		System.out.println("Hello World");
+		App j = new App();
+		j.run();
+	}
+	
+	public void run(){
+		System.out.println("The world is being");
 	}
 
 }
