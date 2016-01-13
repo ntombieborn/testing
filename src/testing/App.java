1a24028428118788300c438e6423570158737ec9
ntombieborn <ntombifuthi.mbatha4@gmail.com>
2016-01-13 13:50:55 +0200

    Added Runner

--- src/testing/App.java
+++ src/testing/App.java
@@ -7,10 +7,15 @@ public class App {
 		System.out.println("Hello World");
 		App j = new App();
 		j.run();
+		j.runner();
 	}
 	
 	public void run(){
 		System.out.println("The world is being");
+	}
+	
+	public void runner(){
+		System.out.println("go far");
 	}
 
 }
